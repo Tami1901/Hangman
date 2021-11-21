e1ae473868808ab9c4b2b680675a36a0dd816bec
Tamara <tamara.luzija@pixion.co>
2021-11-21 13:29:19 +0100

    header fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,20 @@
 import { AddIcon, ArrowBackIcon } from '@chakra-ui/icons';
-import { Avatar, Box, Button, Flex, Heading, HStack } from '@chakra-ui/react';
-import { LinkIconButton } from 'chakra-next-link';
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  SimpleGrid,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { LinkButton, LinkIconButton } from 'chakra-next-link';
 import { redirect } from 'next/dist/server/api-utils';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
@@ -14,51 +28,58 @@ const Header = () => {
   const dispatch = useAppDispatch();
   const { nickname } = useAppSelector((state) => state.nickname);
 
+  const headerColor = useColorModeValue('gray.600', 'gray.400');
+
   const router = useRouter();
+  const logout = () => {
+    dispatch({ type: 'reset-store' });
+    router.push('/');
+  };
 
   return (
-    <Flex
-      as="header"
-      position="relative"
-      justifyContent="space-around"
-      alignItems="center"
-      opacity="0.8"
-      boxShadow="rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px"
-      p="4"
-    >
-      {nickname && (
-        <HStack position="absolute" left="4">
+    <SimpleGrid columns={3} as="header" boxShadow="rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px" p="4">
+      <HStack>
+        {router.pathname === '/scores' && (
           <LinkIconButton
             aria-label="back"
             icon={<ArrowBackIcon />}
-            colorScheme="red"
+            href="/hangman"
+            colorScheme="gray"
             size="md"
-            onClick={() => {
-              dispatch({ type: 'reset-store' });
-              router.push('/');
-            }}
           />
-
-          <Button
-            onClick={() => {
-              router.push('/hangman');
-              dispatch(getQuote());
-              dispatch(refresh());
-            }}
-            colorScheme="orange"
-            size="md"
-          >
-            New
-          </Button>
-        </HStack>
-      )}
-
-      <Heading>Hangman</Heading>
-      <HStack position="absolute" right="4">
-        {nickname && <Avatar name={nickname} size="sm" />}
+        )}
         <ThemeSwitcher />
       </HStack>
-    </Flex>
+
+      <Heading color={headerColor} marginX={'auto'}>
+        Hangman
+      </Heading>
+      <HStack position="absolute" right="4">
+        {nickname && (
+          <Box>
+            <Menu>
+              <MenuButton>
+                <Avatar name={nickname} size="md" />
+              </MenuButton>
+              <MenuList>
+                <MenuItem
+                  onClick={() => {
+                    router.push('/hangman');
+                    dispatch(getQuote());
+                    dispatch(refresh());
+                  }}
+                >
+                  New game
+                </MenuItem>
+                <MenuItem onClick={() => router.push('/scores')}>Scores</MenuItem>
+                <MenuDivider />
+                <MenuItem onClick={() => logout()}>Logout</MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+        )}
+      </HStack>
+    </SimpleGrid>
   );
 };
 
